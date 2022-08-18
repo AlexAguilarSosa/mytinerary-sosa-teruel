242e9385e1ecfb4a4eef842e0714af68116723c1
Teje8 <jteruel8@gmail.com>
2022-08-18 18:55:59 -0300

    create Hero page

---
+++ src/pages/HeroPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/HeroPage.css'
+
+
+export default function HeroPage() {
+    return (
+        <div className='HeroContain'>
+            <h1 className='HeroH1'>MyTinerary</h1>
+            <h3 className='HeroH3'>Discover Unexpected</h3>
+            <button className='HeroButton'>ENTER</button>
+        </div>
+    )
+}
