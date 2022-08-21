493805bbc9f59e59129c9572ea714b6fd77898a0
Teje8 <jteruel8@gmail.com>
2022-08-20 22:39:59 -0300

    add call to action component

---
+++ src/components/CallToAction.js
@@ -0,0 +1,9 @@
+function callToAction (){
+    return(
+        <div>
+        <h3 className='HeroH3'>Discover a New World</h3>
+        <a href='#' className='HeroButton'><strong>Show Me!</strong></a>
+        </div>
+    )
+}
+export default callToAction
