fa59195f4a777919f8477cc89dc32ae81c36b040
Alex Sosa <alexaguilar1698@gmail.com>
2022-08-22 01:53:56 -0300

    under construction js

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/UnderConstruction.css'
+
+export default function UnderConstruction() {
+    return (
+        <div className='UnderContainer'>
+            <p className='UnderText'>Sorry</p>
+            <h2 className='UnderSubtitle'>Page Under Construction</h2>
+        </div>
+    )
+}
