53c4219289cd7a80a66cc0161eecd14dde62897e
Alex Sosa <alexaguilar1698@gmail.com>
2022-08-22 01:49:28 -0300

    class agregate

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ export default function Footer() {
     return (
             <div className='FooterContainer'>
                 <div>
-                    <img className='FooterImg' src="https://www.pngmart.com/files/22/WhatsApp-Logo-PNG-Isolated-HD-Pictures.png" alt=""/>
-                    <img className='FooterImg Ig' src="https://www.pngmart.com/files/13/Instagram-Logo-PNG-Pic.png" alt=""/>
-                    <img className='FooterImg Face' src="https://www.pngmart.com/files/15/Circle-Facebook-Logo-PNG-Transparent-Image.png" alt=""/>
+                    <a href="#"><img className='FooterImg' src="/svg/whatsapp.svg" alt=""/></a>
+                    <a href="#"><img className='FooterImg' src="/svg/instagram.svg" alt=""/></a>
+                    <a href="#"><img className='FooterImg' src="/svg/facebook.svg" alt=""/></a>
                 </div>
-                <p className='FooterP'>
+                <p className='FooterText'>
                 MyTinerary Sosa-Teruel || Cohort-32
                 </p>
             </div>
