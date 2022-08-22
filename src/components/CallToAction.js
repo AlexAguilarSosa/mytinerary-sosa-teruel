9466cbfa9a2bb6ccc0bb0567c0a646e5a5b6cf3e
Alex Sosa <alexaguilar1698@gmail.com>
2022-08-22 01:48:42 -0300

    class agregate

--- src/components/CallToAction.js
+++ src/components/CallToAction.js
@@ -1,9 +1,11 @@
+import '../styles/CallToAction.css'
+
 function callToAction (){
     return(
-        <div>
-        <h3 className='HeroH3'>Discover a New World</h3>
-        <a href='#' className='HeroButton'><strong>Show Me!</strong></a>
-        </div>
+        <>
+        <h3 className='CallH3'>Discover a New World</h3>
+        <a href='#' className='CallButton'><strong>Show Me!</strong></a>
+        </>
     )
 }
 export default callToAction
