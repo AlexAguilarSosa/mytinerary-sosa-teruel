26207cb1064a4e8dc798716ae4ff84a5f6d50b90
Teje8 <jteruel8@gmail.com>
2022-08-23 00:47:19 -0300

    add cities & New city component

--- src/layouts/WebsiteLayout.js
+++ src/layouts/WebsiteLayout.js
@@ -1,11 +1,15 @@
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import Cities from '../components/Cities'
+import NewCity from '../components/NewCity'
 
 function WebsiteLayout(props) {
     return (
         <div>
             <Header />
             { props.children }
+            {/* <Cities /> */}
+            {/* <NewCity /> */}
             <Footer />
         </div>
     )
