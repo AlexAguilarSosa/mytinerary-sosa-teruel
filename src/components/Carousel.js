869aa48da89da29fe0847c34438902b5402810f3
Teje8 <jteruel8@gmail.com>
2022-08-23 00:42:44 -0300

    add interval

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,14 @@
-import React from 'react'
 import '../styles/Carousel.css'
+import ArrowsCarousel from './ArrowsCarousel'
+import { useEffect ,useState } from 'react'
 
-export default function Carousel(props) {
+function Carousel(props) {
     const cities = props.data
     const range = props.range
-    const start = 0
-    const end = start + range
+    const [start, setStart] = useState(0)
+    const [end, setEnd] = useState(start + range)
+    const [intervalId, setIntervalId] = useState()
+    const interval = props.interval * 1000
 
     const cityView = (city) =>(
         <div className='CarouselEvent'>
@@ -14,12 +17,43 @@ export default function Carousel(props) {
         </div>
     )
 
+        useEffect(() => {
+            let id = setInterval(function () {
+                next()
+            }, interval)
+
+            setIntervalId(id)
+
+            return () => clearInterval(intervalId);
+
+        }, [start])
+
+
+        function previous (){
+            if(start >= range){
+            setStart(start - range)
+            setEnd(end - range)
+        }
+    }
+
+    function next (){
+        if(end < cities.length){
+            setStart(start + range)
+            setEnd(end + range)
+        }
+    }
+
+
     return (
         <div className='CarouselContainer'>
             <h2 className='CarouselTitle'>Popular MyTineraries</h2>
             <div className='CarouselSlide'>
-                {cities.slice(start, end).map(cityView)}
+                <ArrowsCarousel icon={"<"} click={previous} />
+                <div className='CarouselContainerImg'>{cities.slice(start, end).map(cityView)}</div>
+                <ArrowsCarousel icon={">"} click={next} />
             </div>
         </div>
     )
 }
+
+export default Carousel
