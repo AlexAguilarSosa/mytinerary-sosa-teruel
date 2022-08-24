3c9663efb66979cc8850d4408c8d60df38b9ac88
Alex Sosa <alexaguilar1698@gmail.com>
2022-08-24 12:14:12 -0300

    routes agregadas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,29 @@
 import '../styles/Footer.css'
+import {Link as LinkRouter} from  'react-router-dom'
 
 export default function Footer() {
+    let Pages = [
+        {name: 'Home', to: '/'},
+        {name: 'Cities', to: '/cities'},
+        {name: 'New City', to: '/newcity'},
+    ]
+
     return (
             <div className='FooterContainer'>
-                <div>
-                    <a href="#"><img className='FooterImg' src="/svg/whatsapp.svg" alt=""/></a>
-                    <a href="#"><img className='FooterImg' src="/svg/instagram.svg" alt=""/></a>
-                    <a href="#"><img className='FooterImg' src="/svg/facebook.svg" alt=""/></a>
+                <div className='FooterTop'>
+                    <div className='FooterNav'>
+                        <h3 className='FooterH3'>Useful Links :</h3>
+                        {Pages.map(page => <LinkRouter to={page.to} className='FooterLink'>{page.name}</LinkRouter>)}
+                    </div>
+                    <div className='FooterSocialLink'>
+                        <LinkRouter to="#"><img className='FooterImg' src="/svg/whatsapp.svg" alt=""/></LinkRouter>
+                        <LinkRouter to="#"><img className='FooterImg' src="/svg/instagram.svg" alt=""/></LinkRouter>
+                        <LinkRouter to="#"><img className='FooterImg' src="/svg/facebook.svg" alt=""/></LinkRouter>
+                    </div>
                 </div>
-                <p className='FooterText'>
-                MyTinerary Sosa-Teruel || Cohort-32
-                </p>
+                <div>
+                    <p className='FooterText'>MyTinerary Sosa-Teruel || Cohort-32</p>
+                </div>
             </div>
     )
 }
