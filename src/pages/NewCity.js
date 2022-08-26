ff0dcc4b5f301aa30f6e0cd5199240bcb5855bf2
Teje8 <jteruel8@gmail.com>
2022-08-26 20:08:29 -0300

    import input component

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -1,10 +1,12 @@
 import '../styles/NewCity.css'
+import Input from '../components/Input.js'
 
 function newCity (){
     return(
             <div className="newCityContainer">
                 <h1 className='NewCityH1'>New City</h1>
                 <div className="mainNewCity">
+                    <Input className='InputNewCity'/>
                 </div>
             </div>
     )
