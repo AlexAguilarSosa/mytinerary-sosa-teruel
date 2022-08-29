4e8c6119a3447f9847de0fcdb7416f52297afd1c
Alex Sosa <alexaguilar1698@gmail.com>
2022-08-29 18:46:08 -0300

    styles front

--- src/pages/NewCity.js
+++ src/pages/NewCity.js
@@ -3,11 +3,17 @@ import Input from '../components/Input.js'
 
 function newCity (){
     return(
-            <div className="newCityContainer">
+            <div className="NewCityContainer">
                 <h1 className='NewCityH1'>New City</h1>
-                <div className="mainNewCity">
-                    <Input />
+                <div className='FormImgContainer'>
+                    <div className="MainNewCity">
+                        <Input />
+                    </div>
+                    <div className='NewCityImgContainer'>
+                        <img className='NewCityImg' src="/svg/travelImg.svg" alt="" /> 
+                    </div>
                 </div>
+                
             </div>
     )
 }
