17b6bc0afd241df817017d605343a2b6b49e835e
Teje8 <jteruel8@gmail.com>
2022-08-31 11:46:26 -0300

    change group name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,7 @@ export default function Footer() {
                     </div>
                 </div>
                 <div className='FooterPImg'>
-                    <p className='FooterText'>Sosa-Teruel {fecha.getFullYear()} &copy; All Rights Reserved</p>
+                    <p className='FooterText'>TERESA {fecha.getFullYear()} &copy; All Rights Reserved</p>
                     <button onClick={ButtonToTop} className='FooterButton'><img src="/svg/upArrow.svg" alt="" /></button>
                 </div>
             </div>
