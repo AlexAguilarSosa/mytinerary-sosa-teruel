fdf13d81f36c234418c99808088c55bb544fd4ba
Teje8 <jteruel8@gmail.com>
2022-08-31 18:54:40 -0300

    add hooks and option component

--- src/components/EditCity.js
+++ src/components/EditCity.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Input from './Input'
 import '../styles/EditCity.css'
+import axios  from 'axios'
+import { useEffect ,useState } from 'react'
+import Option from './Option'
 
 function EditCity() {
+    const [cities, setCities] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:4000/cities/")
+        .then(response => setCities(response.data))
+    }, [])
 
     return (
         <div className='SelectContainer'>
             <select className='EditSelect'>
-                <option>option</option>
-                <option>option</option>
-                <option>option</option>
+            {cities.map(city => <Option city = {city} />)}
             </select>
             <Input />
         </div>
