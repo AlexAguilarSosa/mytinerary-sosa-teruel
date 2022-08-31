c9097f6f4e284427e5ee364dddb2a6bd42a5b89e
Alex Sosa <alexaguilar1698@gmail.com>
2022-08-31 11:24:59 -0300

    component City.js added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import UnderConstruction from './components/UnderConstruction';
+import ScrollToTop from './components/ScrollTop';
 import WebsiteLayout from './layouts/WebsiteLayout';
 import HeroPage from './pages/HeroPage';
 import Cities from './pages/Cities';
 import NewCity from './pages/NewCity';
+import City from './components/City';
+import UnderConstruction from './components/UnderConstruction';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import ScrollToTop from './components/ScrollTop';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={<HeroPage />} />
           <Route path='/*' element={<UnderConstruction />} />
           <Route path='/cities' element={<Cities />} />
+          <Route path='/city' element={<City />} />
           <Route path='/newcity' element={<NewCity />} />
       </Routes>
     </WebsiteLayout>
