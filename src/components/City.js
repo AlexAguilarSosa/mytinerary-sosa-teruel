dec3eb074a27e2b2f97f5c50d6771553fec392a3
Teje8 <jteruel8@gmail.com>
2022-09-02 23:00:00 -0300

    add id to city cards

--- src/components/City.js
+++ src/components/City.js
@@ -1,37 +1,57 @@
 import '../styles/City.css'
 import {Link as LinkRouter} from  'react-router-dom'
-// import axios  from 'axios'
-// import { useEffect } from 'react'
+import React, { useEffect, useState} from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import axios  from 'axios'
 
 
 export default function City() {
+    const [city, setCity] = useState({})
+    const params = useParams()
+    const {id} = params
+    const navigate = useNavigate()
 
-    // useEffect(() =>{
-    //     axios.get('http://localhost:4000/cities/id')
-    //         .then(response => (response.data))
-    // }, [])
+    const getCity = async () => {
+        try {
+            const response = await axios.get(`http://localhost:4000/cities/${id}`)
+
+        if (response.data.response){
+            return setCity(response.data.response)
+        }
+        // throw new Error ("Ups! An error ocurred")
+        }catch (error){
+            return console.log(error.message);
+        }
+    }
+
+        console.log(city);
+
+        useEffect(() => {
+            getCity()
+        }, [])
+
 
     return (
     <div className='CityCardContainer'>
         <LinkRouter to='/cities' className='CityBackButton'><img src="/svg/backArrow.svg" alt=""/></LinkRouter>
-        <div className='CityCardContainerTop'>
-            <h1>Ciudad</h1>
-            <h2>Pais</h2>
+        <div className='CityCardContainerTop' style={{backgroundImage: `url(${city.photo})`}}>
+            <h1 className='CityH1'>{city.city}</h1>
+            <h2 className='CityH2'>{city.country}</h2>
         </div>
         <div className='CityCardContainerBot'>
             <div className='CityData'>
-                <h3>population</h3> 
-                <p>num</p>            
+                <p>Population:</p>            
+                <h3>{city.population}</h3> 
             </div>
             <div className='CityData'>
-                <h3>high season</h3>
-                <p>num</p>            
+                <p>High Season:</p>            
+                <h3>{city.highseason}</h3>
             </div>
             <div className='CityData'>
-                <h3>currency</h3>
-                <p>num</p>            
+                <p>Currency:</p>            
+                <h3>{city.currency}</h3>
             </div>
         </div>
     </div>
-  )
+    )
 }
